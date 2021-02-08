8c1283113e3b37b827d442490502eedc47ae3b52
Sargis <sargisske@mail.ru>
2021-02-08 17:46:26 +0400

    add coereData to CrateGroupViewController

--- newCoreData/CrateGroupViewController.swift
+++ newCoreData/CrateGroupViewController.swift
@@ -6,35 +6,45 @@
 //
 
 import UIKit
-
+import CoreData
 class CrateGroupViewController: UIViewController {
 
     
     @IBOutlet var crewNameTextField: UITextField!
     @IBOutlet var playersCountTextField: UITextField!
     @IBOutlet var privateOrPublickSwitch: UISwitch!
-    
+    let request:NSFetchRequest<Crew> = NSFetchRequest<Crew>(entityName: "Crew")
+    let requestPlayer:NSFetchRequest<Player> = NSFetchRequest<Player>(entityName: "Player")
+    var allGroup:[Crew]?
+    var context:NSManagedObjectContext!
+    var switchOn = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cellAppDelegate()
     }
     
-    
+    func cellAppDelegate() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            self.context = context
+            allGroup = try? context.fetch(request)
+        } else {
+            fatalError("can not create context")
+        }
+    }
     
     @IBAction func addGroupButtonPressed(_ sender: UIButton) {
-        
+        let newGroup = Crew(context: context)
+        newGroup.playersCount = Int16(playersCountTextField.text ?? "") ?? 0
+        if privateOrPublickSwitch.isOn == true {
+            switchOn = true
+        }else {
+            switchOn = false
+        }
+        newGroup.privateOrPublic = switchOn
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
